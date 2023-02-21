9ad030f8ced06f437f80b07e3279f53200ed9ff9
BrentGreer404 <yem1546@yahoo.com>
2023-02-21 15:28:11 -0500

    auth complete

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,21 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../secrets');
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization
+  if (!token) {
+    next({status: 401, message: "token required"})
+  } else {
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err) {
+        next({status:401, message: "token invalid"})
+      } else {
+        req.decodedJwt = decoded
+        next()
+      }
+    })
+  }
+
   /*
     IMPLEMENT
 
